a59abd00b6d3fde587cd7bccb30a9fe37049c4ae
RobbyLi <93488148+Lesdabit@users.noreply.github.com>
2024-05-01 13:47:41 +0800

    update README & tracker (#8)
    * update README
    
    * update tracker
    
    ---------
    
    Co-authored-by: Lesdabit <robbylee0929@gmail.com>

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -113,9 +113,6 @@ class Tracker:
                 color = player.get("team_color",(0,0,255))
                 frame = self.draw_ellipse(frame, player["bbox"],color, track_id)
 
-                if player.get('has_ball',False):
-                    frame = self.draw_traingle(frame, player["bbox"],(0,0,255))
-
             output_video_frames.append(frame)
 
         return output_video_frames
